23bfa65960c5d5582c3e74d60d9fa2293c47a9f5
Mandeepkumarsaw <mandeepkr9386@gmail.com>
2026-01-07 23:40:33 +0530

    Tree tranversal Pre,In,PostOrder with O(n)

--- tree.cpp
+++ tree.cpp
@@ -28,13 +28,55 @@ Node* buildTree(vector<int> preorder) {
     return root;
 }
 
+//PREORDER
+void preordered(Node* root) {
+    if(root == nullptr) return;
+
+    cout<<root->data;
+    preordered(root->left);
+    preordered(root->right);
+}
+
+//INORDER
+void inorder(Node* root) {
+    if(root == NULL) return;
+
+    inorder(root->left);
+    cout<<root->data;
+    inorder(root->right);
+
+}
+
+//POSTORDER
+void postorder(Node* root) {
+    if(root == NULL) return;
+
+    postorder(root->left);
+    postorder(root->right);
+     cout<<root->data;
+}
+
 
 int main(){
      vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
 
      Node* root = buildTree(preorder);
-     cout << root->data <<endl;
-     cout << root->left->data <<endl;
-     cout << root->right->data <<endl;
+     cout << root->data;
+     cout << root->left->data;
+     cout << root->right->data<<endl;
+
+     cout<<"Preorder-";
+     preordered(root);
+     cout<<endl;
+
+     cout<<"Inorder-";
+     inorder(root);
+     cout<<endl;
+
+     cout<<"Postorder-";
+     postorder(root);
+     cout<<endl;
+
+
      return 0;
 }     
