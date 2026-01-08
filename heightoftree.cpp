655bdd5e393bab62945dea52ac6cffbbd287ddbf
Mandeepkumarsaw <mandeepkr9386@gmail.com>
2026-01-08 23:52:24 +0530

    Height pf Tree

---
+++ heightoftree.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+   int data;
+   Node* left;
+   Node* right;
+
+   Node(int val) {
+    data = val;
+    left = right = NULL;
+   }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> preorder) {
+    idx++;
+
+    if(preorder[idx] == -1) return NULL;
+
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);  //LEFT
+    root->right = buildTree(preorder);  //RIGHT
+
+    return root;
+}
+
+//Height of a tree
+int height(Node* root) {
+    if(root == NULL) return 0;
+    
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return max(left,right) + 1;
+};
+
+int main() {
+    vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root = buildTree(preorder);
+
+    cout<<"Height of Tree: "<<height(root)<<endl;
+    return 0;
+}
