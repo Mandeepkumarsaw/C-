c99c324e55784386945191430f14e5919d8f40e2
Mandeepkumarsaw <mandeepkr9386@gmail.com>
2026-01-13 22:30:25 +0530

    Find K(given) Level of Tree O(n)

--- kth_leveltree.cpp
+++ kth_leveltree.cpp
@@ -22,24 +22,24 @@ Node* build(vector<int>& seq){
 
     return root;
 }
-
+    
 void level(Node* root,int k) {
     if(root==NULL) return;
-
+       
     if(k == 1) {
         cout<<root->val<<" ";
         return;
     }
-
+    
     level(root->left,k-1);
     level(root->right,k-1);
-}
-
+}   
+    
 int main() {
     vector<int> order = {1,2,7,-1,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root = build(order);
     int k = 2;
     level(root,k);
-
+    
     return 0;
 }
