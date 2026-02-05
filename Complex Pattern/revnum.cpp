b2575adae38fa782da7e2f5a522c20ec286015c3
Mandeepkumarsaw <153371877+Mandeepkumarsaw@users.noreply.github.com>
2026-02-06 02:41:49 +0530

    Reverse Number

---
+++ Complex Pattern/revnum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int rev(int n){
+    int rev = 0;
+
+	while(n>0){
+		int last = n%10;
+		rev =rev*10+last;
+		n /= 10;
+	}
+
+	return rev;
+}
+
+
+int main() {
+	int n;
+	cin>>n;
+
+    cout<<rev(n);
+	return 0;
+}
