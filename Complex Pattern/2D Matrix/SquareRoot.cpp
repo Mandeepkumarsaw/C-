52a2fa9aa162b71780f410670ff3c66fd52bf85e
Mandeepkumarsaw <153371877+Mandeepkumarsaw@users.noreply.github.com>
2026-04-11 23:56:41 +0530

    Square Root of x

---
+++ Complex Pattern/2D Matrix/SquareRoot.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int squareRoot(int A) {
+        return (int) sqrt(A);
+}
+int main () {
+        int A;
+        cin >> A;
+        cout << squareRoot(A);
+	return 0;
+}
