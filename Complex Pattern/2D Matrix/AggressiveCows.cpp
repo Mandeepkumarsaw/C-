9d8ea135b7988d4377a574be4ee8deac91248bdd
Mandeepkumarsaw <153371877+Mandeepkumarsaw@users.noreply.github.com>
2026-04-13 23:57:24 +0530

    Scenario Based

---
+++ Complex Pattern/2D Matrix/AggressiveCows.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool canPlace(vector<int> &stalls, int n, int c, int dist) {
+    int count = 1;  // first cow at first stall
+    int lastPos = stalls[0];
+
+    for(int i = 1; i < n; i++) {
+        if(stalls[i] - lastPos >= dist) {   //logic of maximum cows
+            count++;
+            lastPos = stalls[i];
+        }
+        if(count >= c) return true;
+    }
+    return false;
+}
+
+int main() {
+    int n, c;
+    cin >> n >> c;
+
+    vector<int> stalls(n);
+    for(int i = 0; i < n; i++) cin >> stalls[i];
+
+    sort(stalls.begin(), stalls.end());
+
+    int low = 1;
+    int high = stalls[n-1] - stalls[0];
+    int ans = 0;
+
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if(canPlace(stalls, n, c, mid)) {
+            ans = mid;      // possible → try bigger distance
+            low = mid + 1;
+        } else {
+            high = mid - 1; // not possible → reduce distance
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
